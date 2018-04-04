d3ff863dab69e4058984bcd13773dcde490a8ad4
Tomás García de Miguel <togademi@gmail.com>
2018-04-04 09:47:40 +0200

    Complete data structure
    Block + Blockchain + Transaction

--- src/blockchain/Blockchain.java
+++ src/blockchain/Blockchain.java
@@ -1,5 +1,10 @@
 package blockchain;
 
 public class Blockchain {
-
+	
+	private int difficulty;		// mining difficulty
+	
+	private int numblocks;		// number of blocks
+	
+	Block block_list[] = new Block[100];	// list of blocks (including genesis)
 }
--- src/blockchain/Transaction.java
+++ src/blockchain/Transaction.java
@@ -1,5 +1,28 @@
 package blockchain;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Transaction {
 
+	public static final int MAX_VALUE = 63;
+	
+	// random number concatenated to the transaction identification (string)
+	private int randomNum = ThreadLocalRandom.current().nextInt(1, MAX_VALUE+1);
+	
+	private int index;		// index of the transaction in the list
+	
+	private String timestamp;	// date of creation of the transaction
+	
+	private String source;		// hexadecimal address of the transmitter
+	
+	private String destination;		// hexadecimal address of the receiver
+	
+	private int sum;	// transaction sum
+		
+	private String signature_source;	// signature of the transmitter
+
+	
+	public String getTransaction(String source, String destination) {
+		return source + "-" + destination + " :" + randomNum;
+	}
 }

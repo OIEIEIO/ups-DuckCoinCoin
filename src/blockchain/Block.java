816c486f51de437288ca1a94c71a6f3e5018ff4b
Tomás García de Miguel <togademi@gmail.com>
2018-04-25 20:09:32 +0200

    Take current timestamp

--- src/blockchain/Block.java
+++ src/blockchain/Block.java
@@ -1,34 +1,39 @@
 package blockchain;
 
+import java.util.Date;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Block {
 	
-	private int index;	// index of the block inside the blockchain
+	private int index;	//index of the block inside the blockchain
 	
-	private String timestamp;	// date of creation of the block
+	private String timestamp;	//date of creation of the block (String)
+	private long longtime;	//date of creation of the block (long)
 	
-	protected String prehash;		// hash of the previous block
+	protected String prehash;		//hash of the previous block
 	
 	Random random = new Random();
 	
-	protected int numtransactions;	// number of transactions
+	protected int numtransactions;	//number of transactions
 	
-	List<Transaction> transaction_list = new ArrayList<Transaction>();	// list of transactions
+	List<Transaction> transaction_list = new ArrayList<Transaction>();	//list of transactions
 	
-	private String roothash;	// root hash of the merkle tree
+	private String roothash;	//root hash of the merkle tree
 	
-	protected String hash;	// hash of the current block
+	protected String hash;	//hash of the current block
 	
-	protected int nonce;	// a field whose value is set so that the hash of the block will contain a run of leading zeros
+	protected int nonce;	//a field whose value is set so that the hash of the block will contain a run of leading zeros
 	
 	public Block() {
 		
-		nonce = 0;
-		timestamp = "2018/04/14 13:16:46";
-		numtransactions = random.nextInt(11);
+		this.nonce = 0;
+		this.longtime = new Date().getTime();
+		this.timestamp = convertTime(longtime);
+		this.numtransactions = random.nextInt(11);
 	//	Transaction transaction_list[] = new Transaction[numtransactions];
 	// TODO transaction a list or an array?
 		List<Transaction> transaction_list = new ArrayList<Transaction>();	// list of transactions
@@ -102,4 +107,9 @@ public class Block {
 		b.setHash(Hash.applySha256(concatenateHash(b, difficulty)));
 	}
 
+	public String convertTime(long time){
+	    Date date = new Date(time);
+	    Format format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    return format.format(date);
+	}
 }
--- src/blockchain/Transaction.java
+++ src/blockchain/Transaction.java
@@ -1,33 +1,46 @@
 package blockchain;
 
+import java.util.Date;
 import java.util.Random;
-import java.time.format.DateTimeFormatter;  
+import java.time.format.DateTimeFormatter;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime; 
 
 public class Transaction {
 
 	public static final int MAX_VALUE = 10;
 	
-	// random number concatenated to the transaction identification (string)
+	//random number concatenated to the transaction identification (string)
 	Random random = new Random();
 	
-	private int index;		// index of the transaction in the list
+	private int index;		//index of the transaction in the list
 	
-	private String timestamp;	// date of creation of the transaction
+	private String timestamp;	//date of creation of the transaction (String)
+	private long longtime;	//date of creation of the transaction (long)
 	
-	public String source;		// hexadecimal address of the transmitter
+	public String source;		//hexadecimal address of the transmitter
 	
-	public String destination;		// hexadecimal address of the receiver
+	public String destination;		//hexadecimal address of the receiver
 	
-	private int sum;	// transaction sum
+	private int sum;	//transaction sum
 		
-	private String signature_source;	// signature of the transmitter
+	private String signature_source;	//signature of the transmitter
 
 	public Transaction(String destination, int sum) {
-		timestamp = "2018/04/14 13:16:46";
+		this.longtime = new Date().getTime();
+		this.timestamp = convertTime(longtime);
+		this.destination = destination;
+		this.sum = sum;
 	}
 	
 	public String getTransaction() {
 		return source + "-" + destination + " :" + random.nextInt(MAX_VALUE);
+	}
+	
+	public String convertTime(long time){
+	    Date date = new Date(time);
+	    Format format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    return format.format(date);
 	}
 }
